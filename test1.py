e2c3998dc1a7e751a84b0e1ee814b527bbcdb001
hqcurious <hqcurious@gmail.com>
2020-11-12 19:31:45 +0800

    Initial commit

---
+++ test1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3.6
+#-*- coding=utf-8 -*-
+#本脚本由许浩齐同学编写！
+#本人QQ:530825212
+#官网：暂无
+print('Welcome to qytang!')
+print('Welcome to qytang!')
+
+test test test test
+test test test test
+test test test test
+test test test test
+test test test test
+test test test test
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
